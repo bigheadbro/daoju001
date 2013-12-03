6068cc6f18893a9a5f37c0d0f9af3341714c7d27
bigheadbro <guichaoqun@gmail.com>
2013-12-03 16:13:10 +0800

    prototype

---
+++ daoju001/Prototype/js/question-edit.js
@@ -0,0 +1,6 @@
+﻿$(function(){
+  $(".add-pic").click(function(){
+    $(".add-question-editor").append("<div><img class=\"img-in-question\" title=\"点击查看原图\" src=\"../img/bird.png\" /></div>");
+  });
+
+});

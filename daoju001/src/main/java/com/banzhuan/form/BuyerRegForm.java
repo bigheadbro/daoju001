9eab7985239e3aceef1ca83f173ceee7ed4c2bcf
bigheadbro <guichaoqun@gmail.com>
2013-12-10 23:00:34 +0800

    buyer reg form

---
+++ daoju001/src/main/java/com/banzhuan/form/BuyerRegForm.java
@@ -0,0 +1,50 @@
+package com.banzhuan.form;
+
+import java.io.Serializable;
+
+public class BuyerRegForm implements Serializable {
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = -2353178323817894578L;
+	private String name;
+	private String pwd;
+	private String email;
+	/**
+	 * @return the name
+	 */
+	public String getName() {
+		return name;
+	}
+	/**
+	 * @param name the name to set
+	 */
+	public void setName(String name) {
+		this.name = name;
+	}
+	/**
+	 * @return the pwd
+	 */
+	public String getPwd() {
+		return pwd;
+	}
+	/**
+	 * @param pwd the pwd to set
+	 */
+	public void setPwd(String pwd) {
+		this.pwd = pwd;
+	}
+	/**
+	 * @return the email
+	 */
+	public String getEmail() {
+		return email;
+	}
+	/**
+	 * @param email the email to set
+	 */
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+}

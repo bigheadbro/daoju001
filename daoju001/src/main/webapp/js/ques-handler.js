5e94d3cbbfa6a77cd67e21b66edee8c2296f165b
bigheadbro <guichaoqun@gmail.com>
2014-01-05 11:45:42 +0800

    question new style

--- daoju001/src/main/webapp/js/ques-handler.js
+++ daoju001/src/main/webapp/js/ques-handler.js
@@ -1,15 +1,22 @@
 $(function() {
 	$(".question-param-li").mouseenter(function(){
-		$(this).children("ul").show();
+		$(this).css("overflow","visible");
+		$(this).css("height","inherit");
+		$(this).css("background","#fff");
+		$(this).children(".sel").css("color","#b8b8b8");
 	}).mouseleave(function(){
-		$(this).children("ul").hide();
+		$(this).css("overflow","hidden");
+		$(this).css("height","21px");
 	});
 	$(".question-param-li ul li").click(function(){
 		$(this).parent().parent().children("input").val($(this).index());
 		$(this).parent().parent().children(".sel").text(this.innerText);
-		$(this).parent().hide();
+		$(this).parent().parent().css("overflow","hidden");
+		$(this).parent().parent().css("height","18px");
+		$(this).parent().parent().css("background","#0088b5");
+		$(this).parent().parent().children(".sel").css("color","#fff");
 	}).mouseenter(function(){
-		$(this).css('background', '#f2f2f2');
+		$(this).css('background', '#7a7a7a');
 	}).mouseleave(function(){
 		$(this).css('background', 'none');
 	});

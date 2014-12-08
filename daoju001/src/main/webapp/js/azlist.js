22a5e020e4ab78ee6c6685cbecdde655150ce8ec
bigheadbro <guichaoqun@gmail.com>
2014-12-08 20:07:14 +0800

    banner详情，供应商页面

--- daoju001/src/main/webapp/js/azlist.js
+++ daoju001/src/main/webapp/js/azlist.js
@@ -5,10 +5,10 @@ $(function() {
 		});
 		$(this).addClass("current");
 		var current = $(this).text();
-		$(".agent-list-content h1").each(function(){
+		$(".provider-list-content h1").each(function(){
 			if($(this).text() == current){
 				$('html, body').animate({
-					scrollTop: $(this).offset().top
+					scrollTop: $(this).offset().top-30
 				},1000);
 
 			}
